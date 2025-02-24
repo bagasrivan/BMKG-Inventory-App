4a35f50053e9948b7bb6fd59d7058100b1ba0dba
Bagaskara Ridho Vandio <bagaskararidhovandio@Bagaskaras-MacBook.local>
2025-02-24 15:46:37 +0800

    Selesai hari Senin 24 Februari

--- lib/inventoryPage.dart
+++ lib/inventoryPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class InventoryPage extends StatefulWidget {
@@ -7,6 +8,33 @@ class InventoryPage extends StatefulWidget {
 }
 
 class _InventoryState extends State<InventoryPage> {
+  List<String> categories = ["Semua", "Tersedia"];
+  String selectedCategory = "Semua";
+
+  List<Map<String, String>> items = [
+    {
+      "name": "Handy Talky",
+      "category": "Peralatan Operasional",
+      "location": "Gudang Operasional",
+      "status": "Tersedia",
+      "image": "assets/ht.png"
+    },
+    {
+      "name": "Mobil Dinas BMKG",
+      "category": "Kendaraan Operasional",
+      "location": "Gudang Stasiun",
+      "status": "Tersedia",
+      "image": "assets/car.png"
+    },
+    {
+      "name": "Printer Canon TS9521C",
+      "category": "Alat Tulis Kantor",
+      "location": "Gudang TU",
+      "status": "Tersedia",
+      "image": "assets/printer.png"
+    },
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -14,6 +42,45 @@ class _InventoryState extends State<InventoryPage> {
         centerTitle: true,
         backgroundColor: Colors.blue[200],
         titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
+          TextField(
+            decoration: InputDecoration(
+                hintText: 'Cari nama barang, barcode, kategori',
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.grey[60],
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none)),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: categories.map(
+                (categories) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: ChoiceChip(
+                        label: Text(categories),
+                        selected: selectedCategory == categories,
+                        selectedColor: Colors.blue[100],
+                        onSelected: (bool selected) {
+                          setState(() {
+                            selectedCategory = categories;
+                          });
+                        }),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
+        ]),
       ),
     );
   }
