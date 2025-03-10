cbb39ec1b111614ead49f7c3031eba34418a05a2
Bagaskara Ridho Vandio <bagaskararidhovandio@Bagaskaras-MacBook.local>
2025-03-10 14:54:50 +0800

    Memindahkan Direktori Main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bmkg_inventory_system/run/main.dart';
+import 'package:bmkg_inventory_system/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
